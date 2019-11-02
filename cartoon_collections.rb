e1da82548163e125237ca13ad4263a5fad1a957b
Nelson A Caudill <nelson.a.caudill@gmail.com>
2019-11-02 12:53:12 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,17 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index{ |dwarf, i| print i+1, dwarf.capitalize }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(summoners)
+  summoners.collect{ |element| "#{element.capitalize}!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any?{ |call| call.size > 4 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(stuff)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheeses = cheese_types.each
+  stuff.find{ |i| cheeses.include?(i) }
 end
