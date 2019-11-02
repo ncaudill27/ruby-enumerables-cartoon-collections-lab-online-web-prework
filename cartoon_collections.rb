02e69bdadf423268080855edd53dda8725809624
Nelson A Caudill <nelson.a.caudill@gmail.com>
2019-11-02 12:11:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index{ |dwarf, i| print i, dwarf.capitalize \n }
 end
 
 def summon_captain_planet# code an argument here
